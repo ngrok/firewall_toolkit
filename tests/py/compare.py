b71d86bc0b3edbb71371500df386cc4dc1f285c9
Mohamed Osman Ahmed <Mosman863@gmail.com>
2023-02-03 17:02:31 +0000

    update compare, update tester and  add more test cases

--- tests/py/compare.py
+++ tests/py/compare.py
@@ -1,18 +1,15 @@
-from typing import Any, Optional
+from typing import Any
 import sys
 import json
 
 ignore: list[str] = [ "metainfo", "handle" ]
 
-def value_error(key: str) -> tuple[int,str]:
+def value_error(key: str) -> tuple[int, str]:
     msg : str = "FAIL: values do not match in " + key
     return (1, msg)
 
-def key_error(keys: Optional[tuple[str,str]]) -> tuple[int,str]:
-    if keys is None:
-        msg: str = "FAIL: key not found in expected"
-    else:
-        msg : str = "FAIL: keys do not match. expected: " + keys[0] + " received: " + keys[1]
+def key_error(keys: tuple[str, str]) -> tuple[int, str]:
+    msg : str = "FAIL: keys do not match. expected: " + keys[0] + " received: " + keys[1]
     return (1, msg)
 
 
@@ -20,9 +17,12 @@ def key_error(keys: Optional[tuple[str,str]]) -> tuple[int,str]:
 def compare(expected: dict[str,Any], received: dict[str,Any]) -> tuple[int,str]:
 
     if len(expected) == 0 or len(received) == 0:
-        return (1, "FAIL: empty dict")
+        return (1, "FAIL: given json of len: 0")
 
-    for expected_key,received_key in zip(expected,received):
+    elif len(expected) != len(received):
+        return (1, "FAIL: given dicts of differnt len")
+
+    for expected_key, received_key in zip(expected, received):
         expected_val: Any = expected[expected_key]
         received_val: Any = received[received_key]
         #key Checking
@@ -30,26 +30,30 @@ def compare(expected: dict[str,Any], received: dict[str,Any]) -> tuple[int,str]:
             case _ as keys if keys[0] != keys[1]:
                 return key_error(keys)
             case _ as keys if keys[0] in ignore:
-                pass
+                continue
         #value checking
         match (expected_val,received_val):
-            case _ as vals if vals[0] != vals[1]:
-                print("here")
-                return value_error(expected_key)
             case _ as vals if type(vals[0]) == type({}):
                 child_dict_ouput: tuple[int,str] = compare(vals[0],vals[1])
                 if child_dict_ouput[0] == 1:
                     return child_dict_ouput
+            case _ as vals if type(vals[0]) == type([]):
+                for v1,v2 in zip(vals[0],vals[1]):
+                    if type(v1) == type({}):
+                         child_dict_ouput: tuple[int,str] = compare(v1,v2)
+                         if child_dict_ouput[0] == 1:
+                             return child_dict_ouput
+                    elif v1 != v2:
+                        return value_error(v1)
+            case _ as vals if vals[0] != vals[1]:
+                return value_error(expected_key)
 
-    for received_key in received:
-        if received_key not in expected:
-            return key_error(None)
 
     return (0,"PASS: exited successfully")
 
 if __name__ == "__main__":
-    expected : dict[str,Any] = json.load(open(sys.argv[1], 'r'))
-    received : dict[str,Any] = json.load(sys.stdin)
-    val: tuple[int,str] = compare(expected,received)
+    expected : dict[str, Any] = json.load(open(sys.argv[1], 'r'))
+    received : dict[str, Any] = json.load(sys.stdin)
+    val: tuple[int, str] = compare(expected, received)
     sys.stdout.write(val[1] + "\n")
     sys.exit(val[0])
--- tests/py/run_test.sh
+++ tests/py/run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+BGreen='\033[1;32m'       # green
+BRed='\033[1;31m'         # Red
 i=0
 j=0
 for e in sample_inputs/*
@@ -7,20 +8,34 @@ do
     echo "${e##*/}"    # print everything after the final "/"
     echo "Comparing ${e##*/} to ${e##*/}"
     cat $e | python3 compare.py $e
-    echo "EXIT CODE: $?"
-    echo "+++"
+    if [[ $? -eq 1 ]]
+    then
+        echo -e "\033[41mEXIT CODE:\033[m 1"
+        echo "===================================="
+    else
+        echo -e "\033[42mEXIT CODE:\033[m 0"
+        echo "===================================="
+    fi
     for r in sample_inputs/*
     do
         if [[ $i -ne $j ]]
         then
             echo "Comparing ${e##*/} to ${r##*/}"
             cat $r | python3 compare.py $e
-            echo "EXIT CODE: $?"
-            echo "+++"
+            if [[ $? -eq 1 ]]
+            then
+                echo -e "\033[41mEXIT CODE:\033[m 1"
+                echo "===================================="
+            else
+                echo -e "\033[42mEXIT CODE:\033[m 0"
+                echo "===================================="
+            fi
+
         fi
         ((j++))
     done
     ((i++))
     j=0
-    echo "=================="
+    read -p "Press enter to continue"
+
 done
