7cf37a015710d92e07ef8a544af1968d2c6468cd
Mohamed Osman Ahmed <Mosman863@gmail.com>
2023-02-07 18:01:38 +0000

    remove old test's test, add new test, add __pycache__ to ignore

--- tests/py/compare.py
+++ tests/py/compare.py
@@ -4,9 +4,6 @@ import json
 
 
 ignore: list[str] = [ "metainfo", "handle" ]
-
-#TODO NEEDS TO BE TYPED IMPORTING OPTIONAL IS NOT WORKING
-#key: Optional[str], keys: Optional[tuple[str,str], expected: Optional[int], received: Optional[int]
 def err_handler(key: Optional[str], keys: Optional[tuple[str,str]], e: Optional[int] , r: Optional[int]) -> tuple[int, str]:
     if key:
         msg : str = "FAIL: values of key: \"" + key + "\" do not match"
@@ -14,19 +11,50 @@ def err_handler(key: Optional[str], keys: Optional[tuple[str,str]], e: Optional[
     elif keys:
         msg : str = "FAIL: keys do not match. expected: \"" + keys[0] + "\" received: \"" + keys[1] + "\""
         return (1, msg)
-    elif expected:
+    elif e:
         return (1, "FAIL: given dicts of differnt len. expected: " + str(e) + " received: " + str(r))
 
-    return (1, "FAIL: given json of len: 0")
+    return (1, "FAIL: given dict of len: 0")
 
 def compare(expected: dict[str,Any], received: dict[str,Any]) -> tuple[int,str]:
+    """Return a tuple(exit_code,exit_message) from the result of comparing 2 dicts
+    >>> compare({ "a" : 1 }, { "a" : 2 })
+    (1, 'FAIL: values of key: "a" do not match')
+    >>> compare({ "metainfo" : 1 , "a" : 2 }, { "metainfo" : 2, "a" : 1 })
+    (1, 'FAIL: values of key: "a" do not match')
+    >>> compare({ "b" : 1 }, { "a" : 1 })
+    (1, 'FAIL: keys do not match. expected: "b" received: "a"')
+    >>> compare({ "metainfo" : 1 , "b" : 1 }, { "metainfo" : 2, "a" : 1 })
+    (1, 'FAIL: keys do not match. expected: "b" received: "a"')
+    >>> compare({ "a" : 1, "b": 1 }, { "a" : 1 })
+    (1, 'FAIL: given dicts of differnt len. expected: 2 received: 1')
+    >>> compare({ "a" : 1 }, {})
+    (1, 'FAIL: given dict of len: 0')
+    >>> compare({}, { "a" : 1 })
+    (1, 'FAIL: given dict of len: 0')
+    >>> compare({ "a": [{1:1},{1:2},{1:1},{1:4}] }, { "a": [{1:1},{1:2},{1:3},{1:4}]})
+    (1, 'FAIL: values of key: "1" do not match')
+    >>> compare({ 1 : { 2 : 2, 4 : 5 }}, { 1 : { 2 : 3, 4 : 5 }})
+    (1, 'FAIL: values of key: "2" do not match')
+    >>> compare({ 1 : { 2 : 3, 4 : 5 }}, { 1 : { 2 : 3, 4 : 5 }})
+    (0, 'PASS: exited successfully')
+    >>> compare({ "a": [{1:1},{1:2},{1:3},{1:4}] }, { "a": [{1:1},{1:2},{1:3},{1:4}]})
+    (0, 'PASS: exited successfully')
+    >>> compare({ "a": [{},{},{},{}] }, { "a": [{},{},{},{}]})
+    (0, 'PASS: exited successfully')
+    >>> compare({ "a" : 1 }, { "a" : 1 })
+    (0, 'PASS: exited successfully')
+    >>> compare({ "metainfo" : 1 , "a" : 1 }, { "metainfo" : 2, "a" : 1 })
+    (0, 'PASS: exited successfully')
+    """
 
-    if len(expected) == 0 or len(received) == 0:
+    if len(expected) == 0 and len(received) == 0:
+        return (0,"PASS: exited successfully")
+    elif len(expected) == 0 or len(received) == 0:
         return err_handler(None,None,None,None)
-
     elif len(expected) != len(received):
         return err_handler(None,None,len(expected),len(received))
-
+    #else
     for expected_key, received_key in zip(expected, received):
         expected_val: Any = expected[expected_key]
         received_val: Any = received[received_key]
@@ -50,10 +78,10 @@ def compare(expected: dict[str,Any], received: dict[str,Any]) -> tuple[int,str]:
                          if child_dict_ouput[0] == 1:
                              return child_dict_ouput
                     elif v1 != v2:
-                        return err_handler(v1,None,None,None)
+                        return err_handler(str(v1),None,None,None)
                         # return value_error(v1)
             case _ as vals if vals[0] != vals[1]:
-                return err_handler(expected_key,None,None,None)
+                return err_handler(str(expected_key),None,None,None)
                 # return value_error(expected_key)
 
     return (0,"PASS: exited successfully")
